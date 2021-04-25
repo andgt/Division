2eba31889952180ed5a63bb6a988d308588b5ce6
Anatoly <anatolijdegtyar@yandex.ru>
2021-04-25 20:49:28 +0300

    Мобильная, планшетная и десктопная версии

--- source/js/script.js
+++ source/js/script.js
@@ -16,3 +16,13 @@ menuToggle.addEventListener("click", function () {
     mainNav.classList.remove("main-nav--opened");
   }
 });
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (mainNav.classList.contains("main-nav--opened")) {
+      evt.preventDefault();
+      mainNav.classList.remove("main-nav--opened");
+      mainNav.classList.add("main-nav--closed");
+    }
+  }
+})
