c987cab64724cb0256e2bc4086318e48acb21a38
Anatoly <anatolijdegtyar@yandex.ru>
2021-04-30 15:14:41 +0300

    Добавляет модальные окна

--- source/js/script.js
+++ source/js/script.js
@@ -25,4 +25,39 @@ window.addEventListener("keydown", function (evt) {
       mainNav.classList.add("main-nav--closed");
     }
   }
-})
+});
+
+// Модальные окна
+
+let modalsOpen = document.querySelectorAll(".js-button-open");
+let modalWindows = document.querySelectorAll(".modal");
+let modalOpenName;
+let buttonsClose = document.querySelectorAll(".js-button-close");
+
+for (let modalOpen of modalsOpen) {
+  modalOpen.onclick = function (evt) {
+    evt.preventDefault();
+    modalOpenName = this.getAttribute("data-modal-window");
+    for (let modalWindow of modalWindows) {
+      if (modalWindow.classList.contains(modalOpenName)) {
+        modalWindow.classList.add("modal__show");
+      }
+    }
+  }
+};
+
+for (let buttonClose of buttonsClose) {
+  buttonClose.onclick = function () {
+    for (let modalWindow of modalWindows) {
+      modalWindow.classList.remove("modal__show");
+    }
+  }
+}
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    for (let modalWindow of modalWindows) {
+      modalWindow.classList.remove("modal__show");
+    }
+  }
+});
