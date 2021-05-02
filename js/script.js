81bc8b9aa392f2d066e2ca75611a31a87d59e1e8
Anatoly <anatolijdegtyar@yandex.ru>
2021-05-02 19:01:56 +0300

    Updates

--- js/script.js
+++ js/script.js
@@ -25,4 +25,59 @@ window.addEventListener("keydown", function (evt) {
       mainNav.classList.add("main-nav--closed");
     }
   }
-})
+});
+
+// Модальные окна
+
+let modal = function () {
+  let htmlPage = document.querySelector(".page");
+  let modalsOpen = document.querySelectorAll(".js-button-open");
+  let modalWindows = document.querySelectorAll(".modal");
+  let modalOverlay = document.querySelector(".modal-overlay");
+  let buttonsClose = document.querySelectorAll(".js-button-close");
+  let modalOpenName;
+  let scrollPosition;
+
+  let closesWindows = function () {
+    for (let modalWindow of modalWindows) {
+      htmlPage.classList.remove("modal__opened");
+      modalWindow.classList.remove("modal__show");
+      modalOverlay.classList.remove("modal-overlay__open");
+      htmlPage.style.scrollBehavior = "auto";
+      window.scrollTo(0, scrollPosition);
+      htmlPage.style.scrollBehavior = "";
+      htmlPage.style.top = "";
+    }
+  };
+
+  for (let modalOpen of modalsOpen) {
+    modalOpen.onclick = function (evt) {
+      evt.preventDefault();
+      scrollPosition = pageYOffset;
+      modalOpenName = this.getAttribute("data-modal-window");
+      for (let modalWindow of modalWindows) {
+        if (modalWindow.classList.contains(modalOpenName)) {
+          htmlPage.classList.add("modal__opened");
+          htmlPage.style.top = -scrollPosition + "px";
+          modalOverlay.classList.add("modal-overlay__open");
+          modalWindow.classList.add("modal__show");
+        }
+      }
+    }
+  };
+
+  for (let buttonClose of buttonsClose) {
+    buttonClose.onclick = function (evt) {
+      evt.preventDefault();
+      closesWindows();
+    }
+  };
+
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      closesWindows();
+    }
+  });
+};
+
+modal();
