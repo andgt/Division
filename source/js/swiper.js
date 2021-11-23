35e7baca8507fdfb04fcd5a7ac57b7f609535f82
anatoliy <anatolijdegtyar@yandex.ru>
2021-11-23 21:29:24 +0300

    Добавляет галерею

---
+++ source/js/swiper.js
@@ -0,0 +1,27 @@
+'use strict'
+
+let gallerySlider = function () {
+  const swiperGallery = new Swiper(".swiper-gallery", {
+    speed: 800,
+    slidesPerView: "auto",
+    spaceBetween: 15,
+    effect: "slide",
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 15,
+      },
+      1440: {
+        slidesPerView: 3,
+        spaceBetween: 15,
+      },
+    }
+  });
+};
+
+gallerySlider();
