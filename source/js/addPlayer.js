4c73671fa683696edeeb0b2d9e6bf078d700632d
anatoly <anatolijdegtyar@yandex.ru>
2022-10-09 20:44:08 +0300

    Дорабатывает добавление игроков и вратарей в табе 'Команда'

--- source/js/addPlayer.js
+++ source/js/addPlayer.js
@@ -2,14 +2,23 @@
 
 let addPlayer = function(evt) {
 	let buttonAdd = document.querySelectorAll('.js-add-player');
-	let boxForTemplate = document.querySelector('.js-players');
+	let boxForPlayers = document.querySelector('.js-players');
+	let boxForGoalkeepers = document.querySelector('.js-goalkeepers');
 	let playerTemplate = document.getElementById('player-template');
 	
 
 	buttonAdd.forEach(el => {
 		el.addEventListener('click', function(e) {
-			let newPlayerField = playerTemplate.content.cloneNode(true);
-			boxForTemplate.appendChild(newPlayerField);
+			let btnValue = el.getAttribute('data-btn');
+
+			if (btnValue === "player") {
+				let newPlayerField = playerTemplate.content.cloneNode(true);
+				boxForPlayers.appendChild(newPlayerField);
+			} else if (btnValue === "goalkeeper") {
+				let newPlayerField = playerTemplate.content.cloneNode(true);
+				boxForGoalkeepers.appendChild(newPlayerField);
+			}
+
 		});
 	})
 };
