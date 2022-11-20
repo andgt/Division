5b97aa13d87e5fcb2eebd454ed7f57daf9f07f45
anatoly <anatolijdegtyar@yandex.ru>
2022-11-20 15:48:31 +0300

    Updates

--- js/selectHandler.js
+++ js/selectHandler.js
@@ -1,18 +1,27 @@
 'use strict'
 
 let changeModel = () => {
+
 	let selectModel = document.querySelectorAll('.js-select-model');
 	let sportModel = document.querySelectorAll('.js-model');
 
 	selectModel.forEach(el => {
 		el.addEventListener('change', () => {
 			let selectValue = el.value;
+			let currentParent;
 
 			sportModel.forEach(model => {
-				
-				model.classList.remove('viewer__active');
 
 				if (model.classList.contains(selectValue)) {
+
+					currentParent = model.parentElement;
+					let removeActive = currentParent.children;
+
+
+					for (let elem of removeActive) {
+						elem.classList.remove('viewer__active')					
+					};
+
 					model.classList.add('viewer__active');
 				}
 
